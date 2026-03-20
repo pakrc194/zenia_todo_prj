a884876216ba0648ded0ebd1b8734f3544c26c2a
pakrc194 <pakrc194@gmail.com>
2026-03-20 14:32:42 +0900

    init prj structure

--- backend/src/main/java/com/example/todo/TodoApplication.java
+++ backend/src/main/java/com/example/todo/TodoApplication.java
@@ -2,12 +2,21 @@ package com.example.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
+		System.out.println("Working dir: " + System.getProperty("user.dir"));
+		
+		SpringApplication app = new SpringApplication(TodoApplication.class);
+        ConfigurableApplicationContext ctx = app.run(args);
+		
+		Environment env = ctx.getEnvironment();
+        System.out.println("▶ DB_URL  : " + env.getProperty("DB_URL"));
+        System.out.println("▶ DB_USER : " + env.getProperty("DB_USER"));
+        System.out.println("▶ DB_PW   : " + env.getProperty("DB_PW"));
 	}
-
 }
